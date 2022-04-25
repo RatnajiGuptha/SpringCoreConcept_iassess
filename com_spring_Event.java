12b55ab9cf9a2a30687cfe423725c970afb63495
rg819 <96975314+RatnajiGuptha@users.noreply.github.com>
2022-04-25 21:51:24 +0530

    event

---
+++ com_spring_Event.java
@@ -0,0 +1,38 @@
+package com.spring;
+
+public class Event {
+	
+	int id;
+	String eventName;
+	Owner eventOrganiser;
+	public Event() {
+		System.out.println("...Creating a new event...");
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEventName() {
+		return eventName;
+	}
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
+	public Owner getEventOrganiser() {
+		return eventOrganiser;
+	}
+	public void setEventOrganiser(Owner eventOrganiser) {
+		this.eventOrganiser = eventOrganiser;
+	}
+	public void display() {
+		//fill the code
+		System.out.println(id+")"+eventName+" - "+eventOrganiser.getName());
+	}
+	public void setProperties(String eventName) {
+		this.setEventName(eventName);
+		
+	}
+
+}
